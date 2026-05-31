cf9077ae179d849d5d6216178a08a3d77bb8abac
mnacik1988 <mnacik1988@gmail.com>
2026-05-31 13:02:23 +0300

    Update service worker cache name and structure

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = 'kapital-app-v0-0-21-unified-order-v1';
+const CACHE_NAME = 'kapital-app-v0-0-21-section-performance-v1';
 const APP_SHELL = [
   './',
   './Depozit_v0_0_21.html',
