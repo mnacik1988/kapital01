80c0bb1ed4d86e6b29f6128a123059039d158b35
mnacik1988 <mnacik1988@gmail.com>
2026-06-02 09:57:35 +0300

    Update service worker cache name and logic

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kapital-app-v0-0-21-bottom-panel-icons-alpha';
+const CACHE_NAME = 'kapital-app-v0-0-21-stocks-polish-v1';
 const APP_SHELL = [
   './',
   './Depozit_v0_0_21.html',
