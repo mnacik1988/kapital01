9a1f7f47994fb8320a0fe43eae256649715d6f8d
mnacik1988 <mnacik1988@gmail.com>
2026-06-03 09:19:58 +0300

    Update service worker cache version and logic

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kapital-app-v0-0-21-hourly-sparklines-v1';
+const CACHE_NAME = 'kapital-app-v0-0-21-hourly-sparklines-v2';
 const APP_SHELL = [
   './',
   './Depozit_v0_0_21.html',
