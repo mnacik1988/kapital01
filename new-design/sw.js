ab5f57953dc69a0d18a5b3bf599babf2f40b5f64
mnacik1988 <mnacik1988@gmail.com>
2026-06-04 10:29:22 +0300

    Add files via upload

---
+++ new-design/sw.js
@@ -0,0 +1,50 @@
+const CACHE_PREFIX = 'kapital-new-design-';
+const CACHE_NAME = CACHE_PREFIX + 'v1';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './panel-icons/1.png?v=4',
+  './panel-icons/2.png?v=4',
+  './panel-icons/3.png?v=4',
+  './panel-icons/4.png?v=4',
+  './panel-icons/5.png?v=4'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_SHELL))
+      .catch(() => null)
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys
+        .filter(key => key.startsWith(CACHE_PREFIX) && key !== CACHE_NAME)
+        .map(key => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+  const url = new URL(req.url);
+
+  if (url.origin === location.origin) {
+    event.respondWith(
+      caches.match(req).then(cached => cached || fetch(req).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(req, copy));
+        return res;
+      }).catch(() => caches.match('./index.html')))
+    );
+    return;
+  }
+
+  event.respondWith(fetch(req).catch(() => caches.match(req)));
+});
