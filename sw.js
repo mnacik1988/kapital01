dd0b43f071d2d6027b5e0399d451e0968ca3e214
mnacik1988 <mnacik1988@gmail.com>
2026-06-08 09:59:51 +0300

    Update service worker cache name and assets

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kapital-app-v0-0-22-liability-button-v1';
+const CACHE_NAME = 'kapital-app-v0-0-24-position-delete-confirm-v1';
 const APP_SHELL = [
   './',
   './Depozit_v0_0_21.html',
