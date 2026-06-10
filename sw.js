5acd5f0c2caa2fae8b492a2d9faec455db8a7af5
mnacik1988 <mnacik1988@gmail.com>
2026-06-10 10:55:12 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kapital-app-v0-0-24-deposit-current-balance-v1';
+const CACHE_NAME = 'kapital-app-v0-0-24-deposit-always-current-v1';
 const APP_SHELL = [
   './',
   './Depozit_v0_0_24.html',
