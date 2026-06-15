cd4ecfc99646720bc6c2126130604c32d922e230
mnacik1988 <mnacik1988@gmail.com>
2026-06-15 22:55:42 +0300

    Update service worker cache name and assets

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kapital-app-v0-0-24-main-counter-choices-compact-v1';
+const CACHE_NAME = 'kapital-app-v0-0-24-chart-card-align-v3';
 const APP_SHELL = [
   './',
   './index.html',
