1619d7bd78d3ff1afd4be601163680d5ab1641ce
mnacik1988 <mnacik1988@gmail.com>
2026-06-16 09:34:31 +0300

    Update service worker cache name and assets

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kapital-app-v0-0-24-bottom-nav-counter-colors-v1';
+const CACHE_NAME = 'kapital-app-v0-0-24-counter-select-button-v1';
 const APP_SHELL = [
   './',
   './index.html',
